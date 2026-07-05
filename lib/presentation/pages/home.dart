df51d967e71a4624d6488de4823338f970b5d834
Kagema <reecejames934@gmail.com>
2026-07-05 14:43:04 +0300

    feat: enhance HomePage layout with AppBar and settings button

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home")));
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home")),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            IconButton(
+              onPressed: () {
+                context.go('/settings');
+              },
+              icon: Icon(Icons.settings),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
