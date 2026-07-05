91bd7ec683d7b033df60a12a4f49d6c2f38c31f1
Kagema <reecejames934@gmail.com>
2026-07-05 14:42:51 +0300

    feat: implement reset password functionality and enhance sign-up page with password strength indicator

--- lib/presentation/pages/settings.dart
+++ lib/presentation/pages/settings.dart
@@ -1,10 +1,114 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_starter/core/providers/theme.dart';
+import 'package:go_router/go_router.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends ConsumerWidget {
   const SettingsPage({super.key});
 
   @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var brightness = ref.watch(themeProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+        leading: IconButton(
+          onPressed: () {
+            context.go('/');
+          },
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 400),
+          child: ListView(
+            children: [
+              _SingleSection(
+                title: "General",
+                children: [
+                  const _CustomListTile(
+                    title: "About Phone",
+                    icon: CupertinoIcons.device_phone_portrait,
+                  ),
+                  _CustomListTile(
+                    title: "Dark Mode",
+                    icon: CupertinoIcons.moon,
+                    trailing: CupertinoSwitch(
+                      value: brightness == ThemeMode.dark ? true : false,
+                      onChanged: (value) {
+                        ref.read(themeProvider.notifier).toggleTheme();
+                      },
+                    ),
+                  ),
+                  const _CustomListTile(
+                    title: "System Apps Updater",
+                    icon: CupertinoIcons.cloud_download,
+                  ),
+                  const _CustomListTile(
+                    title: "Security Status",
+                    icon: CupertinoIcons.lock_shield,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Widget? trailing;
+  const _CustomListTile({
+    required this.title,
+    required this.icon,
+    this.trailing,
+  });
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home")));
+    return ListTile(
+      title: Text(title),
+      leading: Icon(icon),
+      trailing: trailing ?? const Icon(CupertinoIcons.forward, size: 18),
+      onTap: () {},
+    );
+  }
+}
+
+class _SingleSection extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+  const _SingleSection({required this.title, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 16),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title.toUpperCase(),
+            style: Theme.of(
+              context,
+            ).textTheme.headlineMedium?.copyWith(fontSize: 16),
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          color: Theme.of(context).primaryColor.withValues(alpha: 0.05),
+          child: Column(children: children),
+        ),
+      ],
+    );
   }
 }
